30528a21a99a480c3183d9394a7c5835e5dfd6e4
BrenSeibel <brens.seibel@gmail.com>
2020-11-06 19:50:13 -0500

    wk5

--- week5/client/src/index.js
+++ week5/client/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import UserProvider from './context/UserProvider.js'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// react-router-dom
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <UserProvider>
+  <App />
+  </UserProvider>
+  </BrowserRouter>, 
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
